dd58552fa886cd64efd1939d3614f13baf58e15f
carluandraster <andres.efstratiadis@gmail.com>
2025-09-16 10:48:09 -0300

    Update TestConversor.java

--- DeNumeroALetra/src/test/TestConversor.java
+++ DeNumeroALetra/src/test/TestConversor.java
@@ -1,14 +1,21 @@
 package test;
 
-import static org.junit.Assert.*;
-
+import org.junit.Assert;
 import org.junit.Test;
+
+import modelo.Conversor;
 
 public class TestConversor {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testLetraNum() {
+		Conversor c = new Conversor();
+		try {
+			Assert.assertEquals("La conversion no se hizo correctamente", c.letranum("1234.45"), "Mil doscientos treinta y cuatro pesos con 45 centavos");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
