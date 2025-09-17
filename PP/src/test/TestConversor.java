841718df2601b6f7cc9192cc7ab033627e7dcfe3
carluandraster <andres.efstratiadis@gmail.com>
2025-09-17 12:13:27 -0300

    De nuevo

---
+++ PP/src/test/TestConversor.java
@@ -0,0 +1,19 @@
+package test;
+
+import org.junit.Test;
+
+import modelo.Conversor;
+
+public class TestConversor {
+
+	@Test
+	public void testLetraNum() {
+		Conversor c = new Conversor();
+		try {
+			c.letranum("1234.45");
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+}
