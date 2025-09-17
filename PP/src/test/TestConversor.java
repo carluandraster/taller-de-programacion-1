0748581da4d32b8374ec345090bb270df2a2caa5
carluandraster <andres.efstratiadis@gmail.com>
2025-09-17 12:27:13 -0300

    Update TestConversor.java

--- PP/src/test/TestConversor.java
+++ PP/src/test/TestConversor.java
@@ -12,7 +12,7 @@ public class TestConversor {
 		try {
 			c.letranum("1234.45");
 		} catch (Exception e) {
-			System.out.println(e.getMessage());
+			// System.out.println(e.getMessage());
 		}
 	}
 
